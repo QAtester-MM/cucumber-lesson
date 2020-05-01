a46b3e045348df185895ae0ac40137db4be7fdcd
Vladimir <ovodenko@gmail.com>
2020-05-01 16:35:41 +0300

    cucumber steps

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,55 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
 
 public class StepDefinitions {
+
+
+    private int hours;
+
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
+
+        System.out.println("step given: eat cuke " + cukes );
     }
+
+
+    @When("I wait {int} hour")
+    public void I_wait_few_hours(int hours) {
+
+        System.out.println( "step two: wait " + hours + " hours");
+    }
+
+
+    @Then("my belly should growl")
+    public void Belly_grow() {
+        System.out.println("step 3");
+        Assert.assertNotNull( hours );
+    }
+
+
+    @Given("^in the menu these items available$")
+    public void intemsMenu(List<String> arg) {
+        // что-то сделать
+        System.out.println("Items got:");
+        for (int i = 0; i < arg.size(); i++){
+            System.out.println( arg.get(i) );
+        }
+    }
+
+    @Then("^this text will be seen$")
+    public void getText( String expectedMessage ){
+        System.out.println("got message: ");
+        System.out.println( expectedMessage);
+    }
+
+
+
 }
--- src/test/resources/io/cucumber/skeleton/belly.feature
+++ src/test/resources/io/cucumber/skeleton/belly.feature
@@ -1,6 +1,23 @@
 Feature: Belly
 
-  Scenario: a few cukes
+  Scenario: a few cukes case 1
     Given I have 42 cukes in my belly
     When I wait 1 hour
     Then my belly should growl
+
+  Scenario: a few cukes case 2
+    Given I have 22 cukes in my belly
+    When I wait 11 hour
+    Then my belly should growl
+
+  Scenario: test items Menu
+    Given in the menu these items available
+      | File    |
+      | Edit    |
+      | About   |
+    Then this text will be seen
+    """
+    You will have ne e-mail
+    Check new code
+    Send it me
+    """
